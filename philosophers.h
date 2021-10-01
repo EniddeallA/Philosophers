72dd3fa9d3bf5be19c8627c9287828b7598aa5e6
Allaeddine Khalid <akhalid@s2r3p4.1337.ma>
2021-10-01 16:48:05 +0100

    setting up repository

---
+++ philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 15:31:24 by akhalid           #+#    #+#             */
+/*   Updated: 2021/10/01 15:38:59 by akhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
