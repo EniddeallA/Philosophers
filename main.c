a8b30a6e5c0a57a24b7eb8a584666e2e51b92d77
EniddeallA <allaeddinekhalid@gmail.com>
2021-10-23 15:43:09 +0100

    progress

--- main.c
+++ main.c
@@ -6,14 +6,39 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:30:27 by akhalid           #+#    #+#             */
-/*   Updated: 2021/10/01 15:32:47 by akhalid          ###   ########.fr       */
+/*   Updated: 2021/10/23 15:37:30 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+/*
+./a.out number_of_philosophers -- time_to_die -- time_to_eat -- time_to_sleep --
+        [number_of_times_each_philosopher_must_eat]
+*/
+
+void parse_args(int argc,char **argv, t_philo **philo)
+{
+    (*philo)->n_philo = argv[0];
+    (*philo)->die_time = argv[0];
+    (*philo)->eat_time = argv[0];
+    (*philo)->sleep_time = argv[0];
+    if (argc == 5)
+        (*philo)->n_eat_time = argv[0];
+    else
+        (*philo)->n_eat_time = -1;
+}
 
 int main(int argc, char **argv)
 {
+    t_philo *philo;
+
+    if (argc < 4 || argc > 5)
+    {
+        printf("Wrong usage.");
+        return (0);
+    }
+    philo = (t_philo*)malloc(sizeof(t_philo));
+    parse_args(argc, argv, &philo);
     
     return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:31:24 by akhalid           #+#    #+#             */
-/*   Updated: 2021/10/01 15:38:59 by akhalid          ###   ########.fr       */
+/*   Updated: 2021/10/23 15:40:12 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,29 @@
 # include <pthread.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+// SLEEPING 1
+// EATING 2
+// THINKING 3
+
+typedef struct s_state
+{
+    int id;
+    int state;
+    
+}   t_state;
+
+typedef struct s_philo
+{
+    int n_philo;
+    int die_time;
+    int eat_time;
+    int sleep_time;
+    int n_eat_time;
+    t_state state;
+}   t_philo;
+
+void parse_args(int argc,char **argv, t_philo **philo);
+
 
 #endif
