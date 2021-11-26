c48cf962f1ace471b168be59aa45dd3c4f7bd327
EniddeallA <allaeddinekhalid@gmail.com>
2021-11-26 18:16:52 +0100

    Final

---
+++ philo/routine.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 06:40:55 by akhalid           #+#    #+#             */
+/*   Updated: 2021/11/23 16:10:17 by akhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philosophers.h"
+
+void	getting_forks(t_philo *philo)
+{
+	int	pos;
+
+	pos = philo->id - 1;
+	pthread_mutex_lock(&philo->args->fork[pos]);
+	pthread_mutex_lock(&philo->args->print);
+	printf("%ld %d has taken a fork\n", get_time_ms(philo->args->start_time),
+		philo->id);
+	pthread_mutex_unlock(&philo->args->print);
+	pthread_mutex_lock(&philo->args->fork[(pos + 1) % philo->args->n_philo]);
+	pthread_mutex_lock(&philo->args->print);
+	printf("%ld %d has taken a fork\n", get_time_ms(philo->args->start_time),
+		philo->id);
+	pthread_mutex_unlock(&philo->args->print);
+}
+
+void	eating(t_philo *philo)
+{
+	int	pos;
+
+	pos = philo->id - 1;
+	pthread_mutex_lock(&philo->eat);
+	philo->status = 1;
+	pthread_mutex_lock(&philo->args->print);
+	printf("%ld %d is eating\n", get_time_ms(philo->args->start_time),
+		philo->id);
+	pthread_mutex_unlock(&philo->args->print);
+	philo->total_eat += 1;
+	philo->last_eat = get_time_ms(philo->args->start_time);
+	my_usleep(philo->args->eat_time);
+	pthread_mutex_unlock(&philo->args->fork[pos]);
+	pthread_mutex_unlock(&philo->args->fork[(pos + 1) % philo->args->n_philo]);
+	philo->status = 0;
+	pthread_mutex_unlock(&philo->eat);
+}
+
+void	sleeping(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->args->print);
+	printf("%ld %d is sleeping\n", get_time_ms(philo->args->start_time),
+		philo->id);
+	pthread_mutex_unlock(&philo->args->print);
+	my_usleep(philo->args->sleep_time);
+}
+
+void	thinking(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->args->print);
+	printf("%ld %d is thinking\n", get_time_ms(philo->args->start_time),
+		philo->id);
+	pthread_mutex_unlock(&philo->args->print);
+}
---
+++ philo/time.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 00:05:13 by akhalid           #+#    #+#             */
+/*   Updated: 2021/11/23 16:10:23 by akhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philosophers.h"
+
+long	get_time_ms(long time)
+{
+	struct timeval	tp;
+
+	gettimeofday(&tp, NULL);
+	return ((tp.tv_sec * 1000 + tp.tv_usec / 1000) - time);
+}
+
+void	my_usleep(int time)
+{
+	long	current_time;
+
+	current_time = get_time_ms(0);
+	usleep((time * 1e3) - 10000);
+	while (get_time_ms(0) - current_time < time)
+		continue ;
+}
